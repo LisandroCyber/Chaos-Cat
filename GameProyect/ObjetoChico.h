49831ba7216ec58596f2433b42ec5fed775fb966
Lisandro Romero <lisandror94@gmail.com>
2026-09-13 22:12:49 -0300

    feat: permitir lanzar la taza

--- GameProyect/ObjetoChico.h
+++ GameProyect/ObjetoChico.h
@@ -7,6 +7,9 @@ class ObjetoChico
 private:
 	sf::Texture _texture;
 	sf::Sprite _sprite;
+	sf::Vector2f _velocidad;
+	bool _tirado;
+	bool _visible;
 	
 	static sf::Texture cargarTextura();
 
@@ -14,6 +17,10 @@ public:
 	
 	ObjetoChico();
 
+	void update();
+	void tirar(float direccion);
+	void reiniciar();
+	bool estaTirado() const;
 	sf::FloatRect getGlobalBounds() const;
 
 	void draw(sf::RenderTarget& target,
